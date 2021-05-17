c10fc15f799fe4d46d895c4e3e783ba8dd57189c
ElenaKononenko <morra4444@gmail.com>
2021-05-17 17:51:27 +0300

    async

--- contacts.js
+++ contacts.js
@@ -1,92 +1,80 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
-// const __dirname = path.resolve();
-let contactsPath = path.join(path.resolve(), "db", "contacts.json");
+import { v4 as uuid } from "uuid";
+// let contactsPath = path.join(path.resolve(), "db", "contacts.json");
+let contactsPath = path.join("db", "contacts.json");
 
-function listContacts() {
-  fs.readFile(contactsPath, "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error");
-    } else {
-      console.table(JSON.parse(data));
-    }
-  });
-}
-
-function getContactById(id) {
-  fs.readFile(contactsPath, "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error");
-    } else {
-      const arrData = JSON.parse(data);
-      const contact = arrData.find((el) => el.id === +id);
-      console.table(contact);
-    }
-  });
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath, "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error");
-    } else {
-      const arrData = JSON.parse(data);
-      const contacts = arrData.filter((el) => el.id !== +contactId);
-
-      fs.writeFile(contactsPath, JSON.stringify(contacts), (err) => {
-        if (err) {
-          throw err;
-        }
-        console.table(contacts);
-        console.log("Без удаленного контакта");
-      });
-      //   const arrData = JSON.parse(data);
-      //   const contact = arrData.find((el, i) => {
-      //     if (el.id === +contactId) {
-      //       console.log(i);
-      //       arrData.splice(i, 1);
-      //       return el;
-      //     }
-      //   });
-
-      //   fs.writeFile(contactsPath, JSON.stringify(arrData), (err) => {
-      //     if (err) {
-      //       throw err;
-      //     }
-      //     console.table(contact);
-      //     console.log("Удаленный контакт");
-      //   });
-    }
-  });
-}
-
-function addContact(name, email, phone) {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data);
+    console.table(result);
+  } catch (err) {
+    console.log("err :", err);
   }
-  const contact = { id: getRandomInt(11, 98), name, email, phone };
-  fs.readFile(contactsPath, "utf-8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error");
-    } else {
-      const arrData = JSON.parse(data);
-      arrData.push(contact);
-
-      fs.writeFile(contactsPath, JSON.stringify(arrData), (err) => {
-        if (err) {
-          throw err;
-        }
-        console.table(arrData);
-        console.table(contact);
-        console.log("Добавленый контакт");
-      });
-    }
-  });
 }
-export default { listContacts, getContactById, removeContact, addContact };
+
+async function getContactById(id) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const arrData = JSON.parse(data);
+    const contact = arrData.find((el) => el.id === +id);
+    console.table(contact);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const arrData = JSON.parse(data);
+    const contacts = arrData.filter((el) => el.id != contactId);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.table(contacts);
+    console.log("Без удаленного контакта");
+  } catch (err) {
+    console.log(err);
+  }
+
+  //   const arrData = JSON.parse(data);
+  //   const contact = arrData.find((el, i) => {
+  //     if (el.id === +contactId) {
+  //       console.log(i);
+  //       arrData.splice(i, 1);
+  //       return el;
+  //     }
+  //   });
+
+  //   fs.writeFile(contactsPath, JSON.stringify(arrData), (err) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     console.table(contact);
+  //     console.log("Удаленный контакт");
+  //   });
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contact = { id: uuid(), name, email, phone };
+
+    const data = await fs.readFile(contactsPath);
+    const arrData = JSON.parse(data);
+
+    const contactsList = JSON.stringify([...arrData, contact], null, "\t");
+    const add = await fs.writeFile(contactsPath, contactsList);
+
+    console.table(contact);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
